576c94886df9c46a0365bd193fd0844c525f1bc1
yanxx422 <yanxx401@umn.edu>
2022-05-29 21:55:47 -0400

    Split the Image Service into its Own Project

---
+++ starter/catpoint-parent/image-service/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.udacity.catpoint.imageservice{
+  requires org.slf4j;
+  requires software.amazon.awssdk.auth;
+  requires software.amazon.awssdk.core;
+  requires software.amazon.awssdk.regions;
+  requires software.amazon.awssdk.services.rekognition;
+  requires java.desktop;
+  exports com.udacity.catpoint.imageservice.service;
+}
---
+++ starter/catpoint-parent/security-service/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.udacity.catpoint.securityservice{
+  requires com.udacity.catpoint.imageservice;
+  requires java.desktop;
+  requires miglayout;
+  requires com.google.common;
+  requires com.google.gson;
+  requires java.prefs;
+}
