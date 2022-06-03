7bc8887ad0e35f9a4a737c0694f20f31cb39446b
Yan <yanxx401@umn.edu>
2022-06-02 22:11:37 -0400

    Re-structure the project and fix the maven-surefire-plugin configuration error

--- starter/catpoint-parent/image-service/src/main/java/com/udacity/catpoint/imageservice/service/ImageService.java
+++
@@ -1,7 +0,0 @@
-package com.udacity.catpoint.imageservice.service;
-
-import java.awt.image.BufferedImage;
-
-public interface ImageService {
-    boolean imageContainsCat(BufferedImage image, float confidenceThreshhold);
-}
